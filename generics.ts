0267c4f45318c0538bb2828cee562dadb9d97ee5
Sunday <nsoooooooooosn@gmail.com>
2020-05-20 05:32:07 +0700

    Function Generics

--- generics.ts
+++ generics.ts
@@ -21,4 +21,26 @@ class ArrayOfAnything<T> {
   }
 }
 
-new ArrayOfAnything<string>(["a", "b", "c"]);
+const arr = new ArrayOfAnything<string>(["a", "b", "c"]);
+
+// Example of generics with function
+
+function printString(arr: string[]): void {
+  for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+  }
+}
+
+function printNumbers(arr: number[]): void {
+  for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+  }
+}
+
+function printAnything<T>(arr: T[]): void {
+  for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+  }
+}
+
+printAnything<string>(["a", "b", "c"]);
