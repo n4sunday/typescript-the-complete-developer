8883fb795f3781bc8397b2ebd12ca01bce672365
Sunday <nsoooooooooosn@gmail.com>
2020-05-20 05:02:40 +0700

    :tada: Completed Section 11

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,8 @@
-import { CsvFileReader } from "./CsvFileReader";
 import { MatchReader } from "./MatchReader";
-import { ConsoleReport } from "./reportTargets/ConsoleReport";
-import { WinsAnalysis } from "./analyzers/WinsAnalysis";
 import { Summary } from "./Summary";
-import { HtmlReport } from "./reportTargets/HtmlReport";
 
-// Create an Object that satisfies the 'DataReader' interface
-const csvFileReader = new CsvFileReader("football.csv");
-
-// Create an interface of MatchReader and pass in something satisfying
-// the 'DataReader' interface
-const matchReader = new MatchReader(csvFileReader);
+const matchReader = MatchReader.fromCsv("football.csv");
 matchReader.load();
 
-const summary = new Summary(new WinsAnalysis("Man United"), new HtmlReport());
-
+const summary = Summary.winsAnalysisWithHtmlReport("Man United");
 summary.buildAndPrintReport(matchReader.matches);
